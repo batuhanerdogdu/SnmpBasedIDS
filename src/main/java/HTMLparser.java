54b7189197a1846abd6c343e2948054ce1eaf901
batuhanerdogdu <batuhanerdogdu@gmail.com>
2018-02-06 01:34:49 +0300

    parsing some websites for database

--- src/main/java/HTMLparser.java
+++ src/main/java/HTMLparser.java
@@ -6,16 +6,15 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HTMLparser  {
 
     public static void main (String args[]) throws IOException, InterruptedException {
-        //parse symantecs website: https://www.symantec.com/security_response/landing/azlisting.jsp
-        //parse mcafee's website: https://www.mcafee.com/threat-intelligence/malware/latest.aspx?region=us
-        //bad ips: https://www.badips.com/get/list/ssh/3?age=1w
         HTMLparser htmlParser = new HTMLparser();
         //htmlParser.getHTMLcontentOfMalwareDomainListCom();
-        htmlParser.getHMTLcontentOfSymantecCom();
+        //htmlParser.getHTMLcontentOfMcAfeeCom();
     }
 
     //url is the html file's url, part is the required html part (e.g <br>)
@@ -91,24 +90,90 @@ public class HTMLparser  {
         return badIps;
     }
 
+    public ArrayList<String> getHTMLcontentOfMcAfeeCom () throws IOException {
+        ArrayList<String> malwares = new ArrayList<String>();
+        String url = "https://www.mcafee.com/threat-intelligence/malware/latest.aspx?region=us"; //this website wont allow to parse contents
+        Document document = Jsoup.connect(url).timeout(10*10000).get();
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Elements elements = document.getElementsByTag("tbody");
+        Elements rows =  elements.select("tr");
+        System.out.println(elements);
+        return malwares;
+    }
+
     public ArrayList<Malware> getHMTLcontentOfSymantecCom () throws IOException {
         String url = "https://www.mcafee.com/threat-intelligence/malware/latest.aspx?region=us"; //this website wont allow to parse contents
         String url2 ="https://www.symantec.com/security_response/landing/azlisting.jsp";
         Document document = Jsoup.connect(url2).timeout(10*10000).get();
-        Elements elements = document.select("tbody");
+        Elements elements = document.getElementsByTag("tbody");
         Elements rows =  elements.select("tr");
         ArrayList<Malware> malwares = new ArrayList<Malware>();
+        rows.remove(0); //remove the table header row
 
         for (Element row : rows){
             Elements columns = row.getElementsByTag("td");
             ArrayList<String> temp = new ArrayList<String>();
-            Malware m = new Malware();
             for (Element column : columns) {
-                temp.add(column.text());
-                System.out.println (column.text());
+                //System.out.println("-" + column);
+                if (column.text().equals("Symantec AntiVirus detections (1059)") ||
+                        column.text().equals("\n") || column.text().equals("")){
+                    continue;
+                }else if (column.text().startsWith("(These threats are also detected by the latest Virus Definitions.)")){
+                    String tempStr = column.text().replace(
+                            "(These threats are also detected by the latest Virus Definitions.)",
+                            "");
+                    //System.out.println(tempStr);
+                    String str = tempStr.replace(' ', '_');
+                    String regexPattern = "\\S+";
+                    Pattern pattern = Pattern.compile(regexPattern); //"/([A-z])+\\w(?=\")/g"  \S+(?=\")(?<=\")
+                    Matcher matcher = pattern.matcher(str);
+                    while (matcher.find()) {
+                        //System.out.println(matcher.group().substring(0,matcher.group().length()));
+                        temp.add((matcher.group().substring(0, matcher.group().length())));
+                    }
+
+                }else {
+                    //System.out.println("----" + column.text().replace(' ', '_'));
+                    temp.add(column.text().replace(' ', '_'));
+                }
+
             }
-            m.setName(temp.get(0));
-            m.setType(temp.get(1));
+            if (temp.size()==1){
+                Malware m = new Malware();
+                m.setName(temp.get(0));
+                m.setType("Other");
+                m.setDiscoveryDate("Unknown");
+                malwares.add(m);
+            }else if (temp.size()==2){
+                Malware m = new Malware();
+                m.setName(temp.get(0));
+                m.setType(temp.get(1));
+                m.setDiscoveryDate("Unknown");
+                malwares.add(m);
+            }else if (temp.size()==3){
+                Malware m = new Malware();
+                m.setName(temp.get(0));
+                m.setType(temp.get(1));
+                m.setDiscoveryDate(temp.get(2));
+                malwares.add(m);
+            }else if (temp.size()>3){
+                for (String t : temp){
+                    Malware m = new Malware();
+                    m.setName(t);
+                    m.setType("Other");
+                    m.setDiscoveryDate("Unknown");
+                    malwares.add(m);
+                }
+            }
+
+        }
+        for (Malware mal : malwares){
+            System.out.println("Malware Name: "+ mal.getName() + "    Type: "+ mal.getType()+ " Discovery date: "+ mal.getDiscoveryDate());
+            System.out.println("-----------------------------------------------------------------------------------------------");
         }
         return malwares;
     }
